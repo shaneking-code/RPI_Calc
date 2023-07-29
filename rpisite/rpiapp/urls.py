0b7becbb6927f11694eafa512f48b07bee4cc455
Shane King <kingqy@bc.edu>
2023-07-28 20:44:33 -0400

    Began work on templates and urls

--- rpisite/rpiapp/urls.py
+++ rpisite/rpiapp/urls.py
@@ -4,6 +4,18 @@ from . import views
 app_name = "rpiapp"
 urlpatterns = [
     path("", views.index, name="index"),
+    
+    #path("search/leagues/", views.search_leagues, name="search_leagues"),
+    #path("search/teams/", views.search_teams, name="search_teams"),
+    #path("search/games/", views.search_games, name="search_games"),
+    
+    path("leagues/<int:league_id>/", views.league_details, name="league_details"),
+    path("teams/<int:team_id>/", views.team_details, name="team_details"),
+    path("games/<int:game_id>/", views.game_details, name="game_details"),
+
+    path("leagues/<int:league_id>/teams/", views.league_teams, name="league_teams"),
+    path("leagues/<int:game_id>/games/", views.league_games, name="league_games"),
+
     path("<int:season_id>/games/", views.season_results, name="season results"),
     path("<int:season_id>/", views.season_detail, name="season detail")
 ]
--- rpisite/rpiapp/views.py
+++ rpisite/rpiapp/views.py
@@ -1,14 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Game, Season, Team
+from .models import Game, Season, Team, League
 # Create your views here.
 def index(request):
 
     # Show the most recent 5 games
-    header = "<h1> Showing the most recent 5 games: <h1><br>"
     latest_games = Game.objects.all()[:5]
-    output = "<br> ".join([str(game.winner) + " beat " + str(game.loser) + " on " + str(game.date) + " at " + str(game.home_team) for game in latest_games])
-    return HttpResponse(header+output)
+    context = {
+        "latest_games" : latest_games
+    }
+
+    return render(request, "rpiapp/index.html", context)
+
+# Get details of a league through leagues/league_id/
+def league_details(request, league_id):
+
+    league = get_object_or_404(League, id=league_id)
+    context = {
+        "league" : league
+    }
+
+    return render(request, "rpiapp/league_details.html", context)
+
+# Get details of a game through games/game_id
+def game_details(request, game_id):
+
+    game = get_object_or_404(Game, id=game_id)
+    context = {
+        "game" : game
+    }
+
+    return render(request, "rpiapp/game_details.html", context)
+
+# Get details of a team through teams/team_id
+def team_details(request, team_id):
+
+    team = get_object_or_404(Team, id=team_id)
+    team_games = team.home_games.all() | team.away_games.all()
+    team_games = team_games.order_by("date")
+    context = {
+        "team" : team,
+        "team_games" : team_games
+    }
+
+    return render(request, "rpiapp/team_details.html", context)
+
+# Get teams in a league through leagues/league_id/games
+def league_teams(request, league_id):
+
+    league = get_object_or_404(League, id=league_id)
+    teams = league.teams.all()
+    teams = teams.order_by("name")
+    context = {
+        "league" : league,
+        "teams" : teams
+    }
+
+    return render(request, "rpiapp/league_teams.html", context)
+
+# Get games in a league through leagues/league_id/games
+def league_games(request, league_id):
+
+    league = get_object_or_404(League, id=league_id)
+    games = league.games.all()
+    games = games.order_by("date")
+    context = {
+        "league" : league,
+        "games" : games
+    }
+
+    return render(request, "rpiapp/league_games.html", context)
+    
+
 
 def season_detail(request, season_id):
     return HttpResponse(f"Viewing Season {season_id}")
